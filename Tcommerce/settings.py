24479b86c6dbcf28284fefaa28a865bebb5e95d1
mjoghenemaega <mjoghenemaega@gmail.com>
2023-03-16 11:09:28 +0100

    done

--- Tcommerce/settings.py
+++ Tcommerce/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-@m)$ew*agf*&o!zu9n+*5is6eoi9k!ikksauzi8-bk#qn9!)k#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [ 'mjoghenemaega.pythonanywhere.com']
+ALLOWED_HOSTS = ['mjoghenemaega.pythonanywhere.com', 'www.mjoghenemaega.pythonanywhere.com']
+
 
 
 # Application definition
